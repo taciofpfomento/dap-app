25c5768d464ea44a89069efbcfc4d4a1131a6c67
tcamba <tcamba@fpfomento.comm>
2026-02-11 17:48:12 +0100

    feat: Init dap-app project

---
+++ src/router.py
@@ -0,0 +1,28 @@
+from main import app
+
+@app.get("/{adjetivo}")
+def read_root(adjetivo: str):
+    index = f"""
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Prueba API</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  HOLA MUNDO {adjetivo}
+</body>
+
+</html>
+"""
+
+    return HTMLResponse(index)
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
